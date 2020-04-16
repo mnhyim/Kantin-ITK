c8572a1be54ed4bd8d1913af41d8f00fa21240b9
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2020-04-15 20:07:16 -0700

    Add files via upload

---
+++ src/Login.py
@@ -0,0 +1,19 @@
+class Login:
+    def __init__(self):
+        self.username = ""
+        self.password = ""
+        
+    def getUser(self):
+        return self.username
+
+    def getPass(self):
+        return self.password
+    
+    def setUserPass(self, x):
+        self.username = x
+    
+    def setPass(self, x):
+        self.password = x        
+
+
+    
