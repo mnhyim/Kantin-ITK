29b24f5f3a78521e8b3c581163f3acb97a634931
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2020-04-15 20:06:54 -0700

    Update Admin.py

--- src/Admin.py
+++ src/Admin.py
@@ -8,3 +8,26 @@ class Admin:
         self.pswdpembeli = pswdpembeli
         self.userpenjual = userpenjual
         self.pswdpenjual = pswdpenjual
+        
+    #getter & setter
+    def getId(self):
+        return self.idAdmin
+    
+    def setId(self, Id):
+        self.idAdmin = Id
+    
+    def getNama(self):
+        return self.namaAdmin
+    
+    def setNama(self, Nama):
+        self.namaAdmin = Nama
+
+    def tambah_saldo(self,user,saldo):
+        self.user_saldo = user
+        self.jumlah_saldo = self.jumlah_saldo + saldo
+
+    def lihat_user_saldo(self):
+        return self.user_saldo
+
+    def lihat_user_saldo(self):
+        return self.jumlah_saldo
