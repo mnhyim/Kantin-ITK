7cec89013dd1324bf926853de141afc6e098745c
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2020-04-15 20:04:02 -0700

    Add files via upload

---
+++ src/Admin.py
@@ -0,0 +1,10 @@
+class Admin:
+    def __init__(self, idAdmin = None, namaAdmin = None, saldo = None, userpembeli = None, userpembeli = None, pswdpembeli = None, userpenjual = None, pswdpenjual = None):
+        self.idAdmin = idAdmin
+        self.namaAdmin = namAdmin
+        self.user_saldo = user 
+        self.jumlah_saldo = 0
+        self.userpembeli = userpembeli
+        self.pswdpembeli = pswdpembeli
+        self.userpenjual = userpenjual
+        self.pswdpenjual = pswdpenjual
