05e8eedaf81e845ca6db3491eda5cb00fa2f31d8
andisltn48 <61143381+andisltn48@users.noreply.github.com>
2020-04-15 23:36:54 -0700

    Update Login.py

--- src/Login.py
+++ src/Login.py
@@ -2,6 +2,8 @@ class Login:
     def __init__(self):
         self.username = ""
         self.password = ""
+        self.status = ""
+        self.role = ""
         
     def getUser(self):
         return self.username
@@ -15,5 +17,16 @@ class Login:
     def setPass(self, x):
         self.password = x        
 
+    def setStatus(self,x):
+        self.status = x
+
+    def getStatus(self):
+        return self.status
+
+    def setRole(self,x):
+        self.role = x
+
+    def getRole(self):
+        return self.role
 
     
