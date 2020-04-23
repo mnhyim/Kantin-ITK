da28bd5b94c2aaef2cad6efc07ef7a0b8815f9bd
mrizkyar <61149695+mrizkyar@users.noreply.github.com>
2020-04-24 00:47:53 +0800

    Update ItemMenu.py

--- src/ItemMenu.py
+++ src/ItemMenu.py
@@ -1,47 +1,33 @@
 class itemMenu:
-    def __init__(self):
-        self._idMenu = 0
-        self._itemMenu = None
-        self._hargaitem = 0
+    def __init__(self, idMenu, item, hargaitem):
+        self._idMenu = idMenu
+        self._item = item
+        self._hargaitem = hargaitem
 
-    def get_idMenu(self):
+    @property
+    def idMenu(self):
         return self._idMenu
-    def set_idMenu(self, x):
+
+    @idMenu.setter
+    def idMenu(self, x):
         self._idMenu = x
+    @property
+    def item(self):
+        return self._item
+    @item.setter
+    def item(self, x):
+        self._item = x
 
-    def get_itemMenu(self):
-        return self._itemMenu
-    def set_itemMenu(self, x):
-        self._itemMenu = x
-
-    def get_hargaitem(self):
+    @property
+    def hargaitem(self):
         return self._hargaitem
-    def set_hargaitem(self, x):
+    @hargaitem.setter
+    def hargaitem(self, x):
         self._hargaitem = x
 
+    @property
+    def semua(self):
+        return "id Menu : {} \nItem Menu : {} \nharga item : {}".format(self._idMenu,self._item,self.hargaitem)
 
-
-
-
-#
-# im = itemMenu()
-# im.set_idMenu([1,2])
-# im.set_itemMenu ([['Ayam Bakar' , 'Nasi Goreng', 'Rawon'],['batagor','es teh']])
-# im.set_hargaitem ([[15000, 10000 , 10000],[10000,5000]])
-#
-# print(im.get_idMenu())
-# print(im.get_itemMenu())
-# print(im.get_hargaitem())
-
-
-
-
-
-
-
-
-
-
-
-
-
+# im = itemMenu(1, ["Ayam bakar" ,"Rawon" ,"Esteh"], [15000,10000,3000])
+# print(im.semua)
