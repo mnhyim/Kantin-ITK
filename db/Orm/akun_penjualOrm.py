01bd180057b5299974182df12166c52018b92e73
mrizkyar <61149695+mrizkyar@users.noreply.github.com>
2020-04-28 00:48:02 +0800

    Update akun_penjualOrm.py

--- db/Orm/akun_penjualOrm.py
+++ db/Orm/akun_penjualOrm.py
@@ -2,13 +2,16 @@ from sqlalchemy import Column, String, Integer
 from db.base import Base, sessionFactory
 
 
-class akun_penjualOrm(Base):
+class akun_pembeliOrm(Base):
     __tablename__ = 'akun_penjual'
 
-    id = Column(Integer, primary_key = True)
     username = Column(String, unique=True)
-    password = Column(Integer)
+    password = Column(String)
+    idpenjual = Column(Integer, primary_key = True)
+    nama_penjual = Column(String)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password,idpenjual,nama_penjual):
         self.username = username
         self.password = password
+        self.idpembeli = idpenjual
+        self.nama_pembeli = nama_penjual
