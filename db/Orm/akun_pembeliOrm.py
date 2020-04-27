89d54e3f53b9be65ab197a37793954eebf7346b8
mrizkyar <61149695+mrizkyar@users.noreply.github.com>
2020-04-28 00:45:51 +0800

    Update akun_pembeliOrm.py

--- db/Orm/akun_pembeliOrm.py
+++ db/Orm/akun_pembeliOrm.py
@@ -5,10 +5,13 @@ from db.base import Base, sessionFactory
 class akun_pembeliOrm(Base):
     __tablename__ = 'akun_pembeli'
 
-    id = Column(Integer, primary_key = True)
     username = Column(String, unique=True)
-    password = Column(Integer)
+    password = Column(String)
+    idpembeli = Column(Integer, primary_key = True)
+    nama_Pembeli = Column(String)
 
-    def __init__(self, username, password):
+    def __init__(self, username, password,idpembeli,nama_pembeli):
         self.username = username
         self.password = password
+        self.idpembeli = idpembeli
+        self.nama_pembeli = nama_pembeli
