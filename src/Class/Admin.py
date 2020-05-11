c60ce5cd62ce44faf0854aa101ef3810ce0ceb77
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2020-05-11 19:43:04 +0800

    baikin user

--- src/Class/Admin.py
+++ src/Class/Admin.py
@@ -1,12 +1,13 @@
 from src.Class.Akun import Akun
 from src.ORM.ormAkun import AdminOrm
 
+
 class Admin(Akun, AdminOrm):
-    def __init__(self, id, nama, JenisAkun):
-        super().__init__(id, nama, JenisAkun)
     def tambahAkunPembeli(self):
         pass
+
     def tambahAkunPenjual(self):
         pass
+
     def tambahSaldoPembeli(self, obj, x):
         pass
--- src/Class/Pembeli.py
+++ src/Class/Pembeli.py
@@ -2,12 +2,14 @@ from src.Class.Akun import Akun
 from src.ORM.Base import SessionFactory
 from src.ORM.ormAkun import PembeliOrm
 
+
 class Pembeli(Akun, PembeliOrm):
-    def __init__(self, id, nama, JenisAkun, saldo):
-        super().__init__(id, nama, JenisAkun)
+    def __init__(self, saldo, id, nama, email, password, jenisAkun):
+        super().__init__(id, nama, email, password, jenisAkun)
         self.__saldo = saldo
 
     def getSaldo(self):
         return self.__saldo
+
     def setSaldo(self, x):
         self.__saldo = x
