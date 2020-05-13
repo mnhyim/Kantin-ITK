c02716f13be03ea132845d5e7422d099332d350d
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2020-05-13 19:04:06 +0800

    tercerahkan

--- src/Class/Pembeli.py
+++ src/Class/Pembeli.py
@@ -3,9 +3,9 @@ from src.ORM.Base import SessionFactory
 from src.ORM.ormAkun import PembeliOrm
 
 
-class Pembeli(Akun, PembeliOrm):
-    def __init__(self, saldo, id, nama, email, password, jenisAkun):
-        super().__init__(id, nama, email, password, jenisAkun)
+class Pembeli(Akun):
+    def __init__(self, nama, email, password, jenisAkun, saldo):
+        super().__init__(nama, email, password, jenisAkun)
         self.__saldo = saldo
 
     def getSaldo(self):
--- src/Class/Penjual.py
+++ src/Class/Penjual.py
@@ -3,13 +3,20 @@ from src.ORM.Base import SessionFactory
 from src.ORM.ormAkun import PenjualOrm
 
 
-class Penjual(Akun, PenjualOrm):
-    def __init__(self, id, nama, email, password, jenisAkun, saldo):
-        super().__init__(id, nama, email, password, jenisAkun)
+class Penjual(Akun):
+    def __init__(self, nama, email, password, jenisAkun, saldo, menu):
+        super().__init__(nama, email, password, jenisAkun)
         self.__saldo = saldo
+        self.__menu = menu
 
     def getSaldo(self):
         return self.__saldo
 
     def setSaldo(self, x):
         self.__saldo = x
+
+    def getMenu(self):
+        return self.__menu
+
+    def setMenu(self, x):
+        self.__menu.append(x)
