ef027fc957aacafe46b7b1ffb19e4c8a6b99d536
Muhamad Nur Hasyim <mnhyim76@gmail.com>
2020-05-14 10:27:58 +0800

    abs path db

--- src/ORM/Base.py
+++ src/ORM/Base.py
@@ -1,9 +1,14 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
-engine = create_engine('sqlite:///src/ORM/kantin-itk.db')
+# engine = create_engine('sqlite:///src/ORM/kantin-itk.db')
+db_path = os.path.join(os.path.dirname(__file__), 'kantin-itk.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+engine = create_engine(db_uri)
 
 _SessionFactory = sessionmaker(bind=engine)
 
